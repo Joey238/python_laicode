6860709c7e2d34ff567370bb66fa8c94138a52ab
Joey Zhu <joey2378@hotmail.com>
2019-12-03 03:27:53 +0000

    add deque_demo.py

---
+++ Laicode/heap_bfs_5/deque_demo.py
@@ -0,0 +1,9 @@
+from collections import deque
+
+'''
+deque: 
+- stacks and queues (double ended queue0 
+- thread-save, memory efficient appends and pops from either side of the deque O(1)
+'''
+
+d = deque('hello Brea')
